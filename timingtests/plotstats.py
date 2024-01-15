076fec5b29d36e7766d49fcb1a6a335359f44498
Neil Flood <neilflood@fastmail.fm>
2024-01-15 20:11:00 +1000

    Modified to allow multiple moa timing files, for different numbers of CPUs. Also some tidying of graph.

--- timingtests/plotstats.py
+++ timingtests/plotstats.py
@@ -14,7 +14,8 @@ def getCmdargs():
     Get comand line arguments
     """
     p = argparse.ArgumentParser()
-    p.add_argument("--moatiming", help="JSON file of moamosaic timings")
+    p.add_argument("--moatiming", action="append",
+        help="JSON file of moamosaic timings")
     p.add_argument("--gdaltiming", help="JSON file of gdal_merge timings")
     p.add_argument("--plotfile",
         help="Name of output plot file (default plots to screen)")
@@ -30,7 +31,7 @@ def main():
     """
     cmdargs = getCmdargs()
 
-    pyplot.figure(figsize=(8, 4))
+    pyplot.figure(figsize=(9, 5))
     pyplot.subplots_adjust(right=0.98, top=0.98, left=0.08, bottom=0.12)
 
     confidence = cmdargs.confidence
@@ -57,39 +58,47 @@ def plotMoaTimings(cmdargs, lower, upper):
 
     X axis is number of read threads, Y is seconds elapsed time.
     """
-    monitorlist = json.load(open(cmdargs.moatiming))
-    timesByNumthreads = {}
-    for monitor in monitorlist:
-        timestamps = monitor['timestamps']
-        elapsed = timestamps['domosaic:end'] - timestamps['domosaic:start']
-        numthreads = monitor['params']['numthreads']
-        if numthreads not in timesByNumthreads:
-            timesByNumthreads[numthreads] = []
-        timesByNumthreads[numthreads].append(elapsed)
+    maxThreadcount = 0
+    colourList = ['k', 'b', 'g', 'r']
+    j = 0
+    for moatimingfile in cmdargs.moatiming:
+        monitorlist = json.load(open(moatimingfile))
+        timesByNumthreads = {}
+        for monitor in monitorlist:
+            timestamps = monitor['timestamps']
+            elapsed = timestamps['domosaic:end'] - timestamps['domosaic:start']
+            numthreads = monitor['params']['numthreads']
+            if numthreads not in timesByNumthreads:
+                timesByNumthreads[numthreads] = []
+            timesByNumthreads[numthreads].append(elapsed)
 
-    nCpus = monitorlist[0]['params']['cpucount']
+        nCpus = monitorlist[0]['params']['cpucount']
 
-    allNumthreads = numpy.array(list(timesByNumthreads.keys()))
-    numThreadcounts = len(allNumthreads)
-    numStats = 4
-    elapsedStats = numpy.zeros((numThreadcounts, numStats),
-            dtype=numpy.float32)
-    for numthreads in allNumthreads:
-        allElapsed = numpy.array(timesByNumthreads[numthreads])
-        elapsedStats[numthreads - 1, 0] = numpy.percentile(allElapsed, 50)
-        elapsedStats[numthreads - 1, 1] = numpy.percentile(allElapsed, upper)
-        elapsedStats[numthreads - 1, 2] = numpy.percentile(allElapsed, lower)
-        elapsedStats[numthreads - 1, 3] = len(allElapsed)
+        allNumthreads = numpy.array(list(timesByNumthreads.keys()))
+        numThreadcounts = len(allNumthreads)
+        numStats = 4
+        elapsedStats = numpy.zeros((numThreadcounts, numStats),
+                dtype=numpy.float32)
+        for numthreads in allNumthreads:
+            allElapsed = numpy.array(timesByNumthreads[numthreads])
+            elapsedStats[numthreads - 1, 0] = numpy.percentile(allElapsed, 50)
+            elapsedStats[numthreads - 1, 1] = numpy.percentile(allElapsed, upper)
+            elapsedStats[numthreads - 1, 2] = numpy.percentile(allElapsed, lower)
+            elapsedStats[numthreads - 1, 3] = len(allElapsed)
+            maxThreadcount = max(maxThreadcount, numthreads)
 
-    pyplot.plot(allNumthreads, elapsedStats[:, 0], linestyle='-', c='k',
-        label='Moamosaic ({} CPU) (median)'.format(nCpus), linewidth=0.8)
-    pyplot.plot(allNumthreads, elapsedStats[:, 1], linestyle='--', c='k',
-        label='Moamosaic ({} CPU) ({}% confidence)'.format(
-            nCpus, cmdargs.confidence),
-        linewidth=0.8)
-    pyplot.plot(allNumthreads, elapsedStats[:, 2], linestyle='--', c='k',
-        linewidth=0.8)
-    pyplot.xticks(range(numThreadcounts + 1))
+        pyplot.plot(allNumthreads, elapsedStats[:, 0], linestyle='-',
+            label='Moamosaic ({} CPU) (median)'.format(nCpus), linewidth=1.0,
+            c=colourList[j])
+        pyplot.plot(allNumthreads, elapsedStats[:, 1], linestyle='--',
+            label='                                ({}% confidence)'.format(
+                cmdargs.confidence),
+            linewidth=0.5, c=colourList[j])
+        pyplot.plot(allNumthreads, elapsedStats[:, 2], linestyle='--',
+            linewidth=0.5, c=colourList[j])
+
+        j += 1
+    pyplot.xticks(range(maxThreadcount + 1))
 
 
 def plotGdalmergeTimings(cmdargs, lower, upper):
@@ -104,7 +113,7 @@ def plotGdalmergeTimings(cmdargs, lower, upper):
         marker=markers.MarkerStyle('x', fillstyle='none'),
         linewidth=0.8)
     pyplot.scatter([0, 0], quartiles, c='k',
-        label="gdal_merge ({}% confidence)".format(cmdargs.confidence),
+        label="                    ({}% confidence)".format(cmdargs.confidence),
         marker=markers.MarkerStyle('o', fillstyle='none'),
         linewidth=0.8)
 
